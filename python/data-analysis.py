ca986b44bb6412aa6fd1db36c7ad568dae3f77e8
danvelkov <y.vel.yordanov@gmail.com>
2022-11-24 00:09:55 +0200

    removed comments

--- python/data-analysis.py
+++ python/data-analysis.py
@@ -11,31 +11,6 @@ from IPython.display import display
 from datetime import datetime
 from matplotlib.dates import date2num
 
-
-# response = requests.get(sys.argv[1], allow_redirects=True).content
-# current_time = datetime.now()
-# fileName = "buff" + str(datetime.timestamp(current_time)) + ".csv"
-# print(fileName)
-# open(fileName, "wb").write(response)
-
-# df = pd.read_csv(fileName, index_col = 0)
-
-# df.describe()
-# glucose = df[['Timestamp (YYYY-MM-DDThh:mm:ss)', 'Glucose Value (mg/dL)']]
-# glucose = glucose.drop([1,2,3,4,5,6,7,8,9,10])
-# glucose.reset_index(drop=True, inplace=True)
-# glucose['Timestamp (YYYY-MM-DDThh:mm:ss)'] = pd.to_datetime(glucose['Timestamp (YYYY-MM-DDThh:mm:ss)'])
-# glucose = glucose.rename(columns={'Timestamp (YYYY-MM-DDThh:mm:ss)':'Timestamp'})
-# print(glucose.head())
-# print(glucose['Glucose Value (mg/dL)'].mean)
-
-# os.remove(fileName)
-
-
-
-
-
-
 MIN = 70
 MAX = 180
 
